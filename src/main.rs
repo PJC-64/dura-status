7df6d30fe389c10c38dc8f5c7dcde5b66251a52c
Paul Cutmore <paul.cutmore@icloud.com>
2022-02-17 14:17:52 +0000

    First attempt (not working) to call get_process_list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate sysinfo;
+
+use sysinfo::{System, SystemExt};
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut mysys = SystemExt::new_all();
+    mysys.refresh_all();
+
+	let ps = mysys.get_process_list().iter().filter(|(_, p)| p.name.starts_with("dura"));
+	for (pid, p) in ps {
+	    println!("{}:{}", pid, p.name);
+	}
 }
+
