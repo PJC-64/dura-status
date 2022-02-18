cec9658963e38bb87ed6839663a7a226e15d4b7f
Paul Cutmore <paul.cutmore@icloud.com>
2022-02-18 02:50:24 +0000

    Getting process info, but no output?

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 extern crate sysinfo;
-
 use sysinfo::{System, SystemExt};
 
 fn main() {
+    let s = System::new();
+    for process in s.processes_by_name("dura") {
+        println!("Process found: {:?}", process);
+    }
 
-    let mut mysys = SystemExt::new_all();
-    mysys.refresh_all();
-
-	let ps = mysys.get_process_list().iter().filter(|(_, p)| p.name.starts_with("dura"));
-	for (pid, p) in ps {
-	    println!("{}:{}", pid, p.name);
-	}
 }
 
