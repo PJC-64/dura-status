8217bcf96412a594a97bb1ebfb7a8217e6585c6f
Paul Cutmore <paul.cutmore@icloud.com>
2022-02-18 04:14:51 +0000

    Semi-working - picking up dura process & pid.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 extern crate sysinfo;
-use sysinfo::{System, SystemExt};
+use sysinfo::{System, SystemExt, ProcessExt};
 
 fn main() {
-    let s = System::new();
-    for process in s.processes_by_name("dura") {
-        println!("Process found: {:?}", process);
+    let mut s = System::new();
+    s.refresh_all();
+    let procs = s.processes_by_exact_name("dura");
+    for p in procs {
+        println!("Process found: {}, PID: {}\n", p.name(), p.pid());
     }
-
 }
 
