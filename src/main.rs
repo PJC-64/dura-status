f37aa65abe6d16e3aa09bb45b063d50cb79a4734
Paul Cutmore <paul.cutmore@icloud.com>
2022-02-25 15:29:36 +0000

    First-cut version with PID comparisons

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 extern crate sysinfo;
-use sysinfo::{System, SystemExt, ProcessExt};
 
 fn main() {
+    use sysinfo::{System, SystemExt, ProcessExt, PidExt};
+    use std::process;
+
     let mut s = System::new();
     s.refresh_all();
     let procs = s.processes_by_exact_name("dura");
     for p in procs {
-        println!("Process found: {}, PID: {}\n", p.name(), p.pid());
+        if p.pid().as_u32() != process::id() {
+            println!("Process found: {}, PID: {}\n", p.name(), p.pid().as_u32());
+        }
     }
 }
 
